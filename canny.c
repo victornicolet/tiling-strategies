de6b7fbf173f72218b8beb524881dc18d12c5743
victornicolet <victor.nicolet.0@gmail.com>
2015-05-21 12:06:24 +0200

    Building naive pipeline

--- canny.c
+++ canny.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #include <math.h>
 #include <omp.h>
+#include <inttypes.h>
+#include "utils.h"
 
 #define GAUSS_MASK_5(im,x,y) (2.0*(im[x-2][y-2]+im[x+2][y+2]+\
     im[x+2][y-2]+im[x-2][y+2]) +\
@@ -13,15 +15,38 @@
 
 #define GRAD_X(im,x,y) im[x+1][y]-im[x-1][y]
 #define GRAD_Y(im,x,y) im[x][y+1]-im[x][y-1]
-#define GRAD2D(im,x,y) GRAD_Y(im,x,y)+GRAD_X(im,x,y)
+#define GRAD2D(im,x,y) GRAD_Y(im,x,y)+GRAD_X(im,x,y) 
+
+#define LOOP2D(x,y,x0,xn,y0,yn)   for(x = x0; x < xn; x++){ \
+    for(y = y0; y < yn; y++){
+#define END_LOOP2D } }
 
 #define EDGE(im,x,y) atan((GRAD_X(im,x,y))/(GRAD_Y(im,x,y)))
 
-int pipelline_canny(int width, int height,
+int pipeline_canny_naive(int width, int height,
   double ** image, int hit){
+
+  uint8_t x,y;
+
+  // Padding image for gaussian filter
+  MX_ALLOC(image_padded, width + 4, height + 4)
+
+  LOOP2D(x, y, 0, width, 0, height)
+    image_padded[x + 4] = image[x];
+  END_LOOP2D
   // Apply Gaussian filter
-  // Apply gradient intesity
-  // Edge detection
+  MX_ALLOC(image_G, width, height)
+  LOOP2D(x, y, 0, width, 0, height)
+      image_G[x][y] = GAUSS_MASK_5(image, x+4, y+4);
+  END_LOOP2D
+  FREE_MX(image_padded, width + 4)
+  // Apply gradient intensity
+  MX_ALLOC(gradx, width, height)
+  MX_ALLOC(grady, width, height)
+  LOOP2D(x, y, 0, width, height)
+    grady[x][y] = GRAD_Y(image_G, x, y);
+    gradx[x][y] = GRAD_X(image_G, x, y);
+  END_LOOP2D
   // Non-maxima suppression
   // Edge levelling
 }
