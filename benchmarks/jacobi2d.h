fefd1059aef888dae02d3a0582645bc2576ef4f8
victornicolet <victor.nicolet.0@gmail.com>
2015-06-01 14:32:29 +0200

    Format

--- benchmarks/jacobi2d.h
+++ benchmarks/jacobi2d.h
@@ -3,9 +3,7 @@
 #ifndef JACOBI2D_H
 #define JACOBI2D_H
 
-// Debugging
-//#define DEBUG
-
+/* Debugging dimensions */
 #ifdef DEBUG
   #define DBG_SIZE 1024
   #define DBG_ITER 512
@@ -13,20 +11,27 @@
 // App
 //#define SEQ
 #define CHECK_ON_SIZE 8
-// TILE DIMENSIONS -------------------------------
-// Iterations within a tile
+/* TILE DIMENSIONS */
+/* Iterations within a tile */
 #define T_ITERS 32
-// Fill a cache line with 8 doubles or 16 float
+/* Fill a cache line with 8 doubles or 16 float */
 #define T_WIDTH_DBL 32
 #define T_HEIGHT_DBL 32
 #define T_WIDTH_FLT 8
-// Different values for overlapped version
+/* Different values for overlapped version */
 
+/* Stencil macros */
 #define JACOBI2D_T(t,i,j) (t[i][j] + t[i][j-1] + t[i][j+1] + t[i+1][j] + \
   t[i+1][j-1] + t[i+1][j-1] + t[i-1][j-1] + t[i-1][j] + t[i-1][j+1])/9.0
 
+struct jbi2d_args {
+  int X;
+  int Y;
+  int iters;
+  double ** input;
+};
 
-void djbi2d_seq(int, int, double **,struct benchscore *);
+void djbi2d_seq(struct jbi2d_args, struct benchscore *);
 
 
 
